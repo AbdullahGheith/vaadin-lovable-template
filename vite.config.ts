157c1c85881022c9198c9e63139123a91fc2b7f3
Abdullah Gheith <aygheith@gmail.com>
2025-04-14 10:47:34 +0200

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { UserConfigFn } from 'vite';
 import { overrideVaadinConfig } from './vite.generated';
+import {fileURLToPath, URL} from "node:url";
 
 const customConfig: UserConfigFn = (env) => ({
   // Here you can add custom Vite parameters
   // https://vitejs.dev/config/
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src/main/frontend', import.meta.url))
+    }
+  }
 });
 
 export default overrideVaadinConfig(customConfig);
